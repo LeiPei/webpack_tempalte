37c10d9b5bbd5b273206ee0c269ccda85b231956
Leo Pei <lpei@fortinet.com>
2019-06-21 14:40:46 -0700

    First commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './style/site.css'
+import printMe from './js/print.js'
+
+printMe()
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/main.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    open: 'Google Chrome'
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    open: 'Google Chrome'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'playground',
+      template: './src/index_template.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ],
+  },
+};
