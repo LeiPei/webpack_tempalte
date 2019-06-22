f8c4a7ec7dca2d192706cd0083eb8dd5445adcba
Leo Pei <lpei@fortinet.com>
2019-06-21 17:36:19 -0700

    add babel to the project

---
+++ src/credentials/secret.js
@@ -0,0 +1,3 @@
+export default {
+  google_map_key: 'AIzaSyCEtWaWhc3z0_lMq6Gwy_X9o1XCK97wWrQ'
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,52 @@
 import './style/site.css'
 import printMe from './js/print.js'
+import secret from './credentials/secret.js'
 
-printMe()
+const echarts = require('echarts');
+const loadGoogleMapsApi = require('load-google-maps-api');
+
+var myChart = echarts.init(document.getElementById('main'));
+// specify chart configuration item and data
+var option = {
+  title: {
+    text: 'ECharts entry example'
+  },
+  tooltip: {},
+  legend: {
+    data: ['Sales']
+  },
+  xAxis: {
+    data: ["shirt", "cardign", "chiffon shirt", "pants", "heels", "socks"]
+  },
+  yAxis: {},
+  series: [{
+    name: 'Sales',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20]
+  }]
+};
+
+// use configuration item and data specified to show chart
+myChart.setOption(option);
+
+
+// load Google Map
+loadGoogleMapsApi({key: secret.google_map_key}).then(function (googleMaps) {
+  //  google.maps == googleMaps
+  let map = new googleMaps.Map(document.querySelector('#map'), {
+    center: {
+      lat: 40.7484405,
+      lng: -73.9944191
+    },
+    zoom: 12
+  })
+}).catch(function (error) {
+  console.error(error)
+})
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
